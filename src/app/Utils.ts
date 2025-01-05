3888ec068c3245584382da92638bf2aec6ac91b6
Joono Chakma <73777352+joonopohr@users.noreply.github.com>
2025-01-06 00:33:13 +1100

    add assertions and matchers

--- src/app/Utils.ts
+++ src/app/Utils.ts
@@ -1,3 +1,22 @@
 export function toUpperCase(args: string) {
     return args.toUpperCase();
+
+}
+
+export type stringInfo = {
+    lowerCase: string,
+    upperCase: string,
+    characters: string[],
+    length: number,
+    extraInfo: Object | undefined
+}
+
+export function getStringInfo(arg: string): stringInfo {
+    return{
+        lowerCase: arg.toLowerCase(),
+        upperCase: arg.toUpperCase(),
+        characters: Array.from(arg),
+        length: arg.length,
+        extraInfo: {}
+    }
 }
--- src/test/Utils.test.ts
+++ src/test/Utils.test.ts
@@ -1,8 +1,38 @@
-import { toUpperCase } from "../app/Utils";
+import { getStringInfo, toUpperCase } from "../app/Utils";
 describe('Utils test suite', ()=>{
     
-    test('should return upper case string', ()=>{
+    it('should return upper case of a valid string', ()=>{
+        //arrange
+        const sut = toUpperCase;
+        const expected ='ABC'
+        
+        //act
+        const actual = toUpperCase('abc');
+
+        //assert
         const result = toUpperCase('abc');
-        expect(result).toBe('ABC');
+        expect(result).toBe(expected);
     })      
+
+    it.only("shoud return info for a valid string", ()=>{
+        const actual = getStringInfo("My-String");
+        
+        expect(actual.lowerCase).toBe("my-string");
+        expect(actual.extraInfo).toEqual({});
+
+        expect(actual.characters.length).toBe(9);
+        expect(actual.characters).toHaveLength(9);
+
+        expect(actual.characters).toEqual(['M', 'y', '-', 'S', 't', 'r', 'i', 'n', 'g']); 
+        expect(actual.characters).toContain<string>('M');
+        expect(actual.characters).toEqual(
+            expect.arrayContaining(['S', 't', 'r', 'i', 'n', 'g','M', 'y', '-',])
+        );
+
+
+        expect(actual.extraInfo).not.toBe(undefined);
+        expect(actual.extraInfo).not.toBeUndefined();
+        expect(actual.extraInfo).not.toBeDefined();
+        expect(actual.extraInfo).toBeTruthy();
+    });
 });
